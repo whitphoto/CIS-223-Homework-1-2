25c2a6be6dd6712ea3fe28783a3b2fab01efe132
whitphoto <nate@whitchurchphotography.com>
2018-03-30 19:52:32 -0400

    This Homework is your average Hello World program 	using C# and Windows 10 UWP

---
+++ MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿/********************************************************
+*														*
+*		Nathan Whitchurch								*
+*		CIS 223 CIS223: COMPUTER PROJECTS & APPLICATNS	*
+*		Dr Whittle										*
+*		Homework 1-2 Hello World 						*
+*														*
+********************************************************/
+/*
+
+	This Homework is your average Hello World program
+	using C# and Windows 10 UWP 
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+/********************************************************
+*														*
+*		Nathan Whitchurch								*
+*		CIS 223 CIS223: COMPUTER PROJECTS & APPLICATNS	*
+*		Dr Whittle										*
+*		Homework 1-2 Hello World 						*
+*														*
+********************************************************/
+/*
+
+	This Homework is your average Hello World program
+	using C# and Windows 10 UWP 
+
+*/
